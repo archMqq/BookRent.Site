599dcf4e9bc6e909b8b0e4b14be64c270cf7982a
aanswerx <clouztoyou@mail.ru>
2024-08-01 18:04:45 +0300

    Добавьте файлы проекта.

---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace BookRent.Site.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Login {  get; set; }
+        public string Password { get; set; }
+    }
+}
